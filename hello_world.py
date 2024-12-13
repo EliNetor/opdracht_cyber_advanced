9e5552231179dd1330cb3287665b53442f5ab445
EliNetor <eli.vermeulen2002@gmail.com>
2024-12-13 12:36:15 +0100

    test bandit flow

--- hello_world.py
+++ hello_world.py
@@ -1,9 +1,21 @@
-print("Hallo World!")
+# print("Hallo World!")
 
-# password test
-def connect_to_service():
-    password = "test123"
-    print("Connecting to service with password:", password)
+# # password test
+# def connect_to_service():
+#     password = "test123"
+#     print("Connecting to service with password:", password)
+
+# if __name__ == "__main__":
+#     connect_to_service()
+
+# insecure_script.py
+
+import os
+
+def insecure_function():
+    # Insecure: Using os.system() without sanitizing input
+    user_input = input("Enter command: ")
+    os.system(user_input)
 
 if __name__ == "__main__":
-    connect_to_service()
+    insecure_function()
