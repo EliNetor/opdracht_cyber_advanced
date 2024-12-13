1d0fe1cdd8aac3d47ecb196fa953b1d780a51a29
EliNetor <eli.vermeulen2002@gmail.com>
2024-12-13 12:37:43 +0100

    fix script

--- hello_world.py
+++ hello_world.py
@@ -1,15 +1,3 @@
-# print("Hallo World!")
-
-# # password test
-# def connect_to_service():
-#     password = "test123"
-#     print("Connecting to service with password:", password)
-
-# if __name__ == "__main__":
-#     connect_to_service()
-
-# insecure_script.py
-
 import os
 
 def insecure_function():
