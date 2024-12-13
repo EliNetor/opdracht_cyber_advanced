fcff493d749b8f33e6c5b0ec18a69e5ef9039fcd
EliNetor <eli.vermeulen2002@gmail.com>
2024-12-13 11:52:21 +0100

    add test for password detection

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,9 @@
 print("Hallo World!")
+
+# password test
+def connect_to_service():
+    password = "test123"
+    print("Connecting to service with password:", password)
+
+if __name__ == "__main__":
+    connect_to_service()
